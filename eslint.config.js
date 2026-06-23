3934e77600067a0729ce9350a9c3140d68d1e141
yontank <yontank007@gmail.com>
2026-06-23 17:26:03 +0300

    feat(eslint): added eslint for QoL during development

--- eslint.config.js
+++ eslint.config.js
@@ -3,20 +3,94 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintReact from '@eslint-react/eslint-plugin'
+import { importX } from 'eslint-plugin-import-x'
+import noSecrets from 'eslint-plugin-no-secrets'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import json from '@eslint/json'
+import promise from 'eslint-plugin-promise'
+import packageJson from 'eslint-plugin-package-json'
+import regexp from 'eslint-plugin-regexp'
+import sonarjs from 'eslint-plugin-sonarjs'
+
+//NOTE: side-effect import: patches Linter.verify to upgrade all warnings to errors
+import 'eslint-plugin-only-error'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'import-x': importX,
+      'no-secrets': noSecrets,
+      promise: promise
+    },
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite
+      reactRefresh.configs.vite,
+
+      // vite recommendation for prod apps
+      tseslint.configs.recommended,
+      tseslint.configs.stylisticTypeChecked,
+
+      // react eslint plugins
+      eslintReact.configs['recommended-typescript'],
+
+      // import-x
+      'import-x/flat/recommended',
+      //promise plugin
+      promise.configs['flat/recommended'],
+      //regexp
+      regexp.configs.recommended,
+      //sonarjs
+      sonarjs.configs.recommended
     ],
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      },
       globals: globals.browser
+    },
+    rules: {
+      'import-x/no-dynamic-require': 'warn',
+      'import-x/no-nodejs-modules': 'warn',
+      'no-secrets/no-secrets': 'error'
+    }
+  },
+  {
+    plugins: {
+      json,
+      'no-secrets': noSecrets
+    }
+  },
+
+  // lint JSON files and JSON with comments
+  // I want json files to have comments, no matter the file.
+  {
+    files: ['**/*.json', '**/*.jsonc', '.vscode/*.json'],
+    language: 'json/jsonc',
+    rules: {
+      'json/no-duplicate-keys': 'error'
+    }
+  },
+  // lint JSON5 files
+  {
+    files: ['**/*.json5'],
+    language: 'json/json5',
+    rules: {
+      'json/no-duplicate-keys': 'error'
+    }
+  },
+  {
+    extends: [packageJson.configs.recommended],
+    files: ['package.json'],
+    rules: {
+      'package-json/require-author': 'error'
     }
   }
 ])
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.querySelector('#root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
