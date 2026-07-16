18352417392d55f4cea6e47c27a249a144bd15c3
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2026-07-16 17:03:41 +0000

    feat: add shadcn components to project

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+// zod utility function to check if a string value is all numbers
+export function digitsOnly<T extends string>(v: T): boolean {
+  const n = Number(v)
+  return !isNaN(n) && v?.length > 0 && n > 0
+}
